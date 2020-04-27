9db695c168a1f018b5ccf936019b0c1abf6ba5e7
Ruthvikbr <ruthvikbr24@gmail.com>
2020-04-27 19:50:10 +0530

    add firebase auth

--- app/src/main/java/com/loginScreen/sample/LoginActivity.java
+++ app/src/main/java/com/loginScreen/sample/LoginActivity.java
@@ -1,24 +1,32 @@
 package com.loginScreen.sample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+        private FirebaseAuth mAuth;
+    private final String TAG = LoginActivity.this.getLocalClassName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mAuth = FirebaseAuth.getInstance();
         FloatingActionButton backButton = findViewById(R.id.loginBackButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,8 +61,37 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else{
                     //login code
+
                 }
             }
         });
+    }
+    private void login(String email, String password){
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+                // ...
+            }
+        });
+    }
+    private void updateUI(FirebaseUser firebaseUser){
+        if(firebaseUser != null){
+            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/loginScreen/sample/RegisterActivity.java
+++ app/src/main/java/com/loginScreen/sample/RegisterActivity.java
@@ -1,22 +1,32 @@
 package com.loginScreen.sample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private FirebaseAuth auth;
+    private final String TAG = RegisterActivity.this.getLocalClassName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        auth = FirebaseAuth.getInstance();
 
         FloatingActionButton backButton = findViewById(R.id.signUpBackButton);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -50,8 +60,36 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "Password length has to be more than 6", Toast.LENGTH_SHORT).show();
                 } else {
                     //Register Code
+                    signUp(email.getText().toString(),password.getText().toString());
                 }
             }
         });
+    }
+
+
+    private void signUp(String email,String password){
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                    }
+                });
+    }
+    private void updateUI(FirebaseUser firebaseUser){
+        if(firebaseUser != null){
+            Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
